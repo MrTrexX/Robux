97a42be5ce3304d638d6820c9961694f147dc740
MrTrexX <64513583+MrTrexX@users.noreply.github.com>
2020-12-27 21:41:31 +0800

    Add files via upload

---
+++ Robux Giver.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string username;
+	string pass;
+	int amount;
+	
+	cout << "--LOGIN TO ROBLOX--" << endl;
+	cout << "Username: ";
+	cin >> username;
+	cout << "Password: ";
+	cin >> pass;
+	cout << "Amount of robux you want: ";
+	cin >> amount;
+	
+	for(int num = 0; num < amount; num++) {
+		cout << username << " ";
+		cout << pass << endl;
+	}
+	return 0;
+}
